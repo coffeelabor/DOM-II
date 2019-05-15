0d6ad55ddcf97ac7d6f845b1122b67b10c3cf5ca
Reed James <ReedTJames1234@gmail.com>
2019-05-15 16:13:34 -0500

    WIP: still working on this

--- js/index.js
+++ js/index.js
@@ -12,3 +12,10 @@ two.addEventListener("dblclick", function(event) {
   event.target.style.backgroundColor = "black";
   event.target.style.color = "white";
 });
+
+const three = document.getElementsByTagName("body");
+three.addEventListener("mousemove", runEventThree);
+
+function runEventThree(event) {
+  console.log(event.offsetX, event.offsetY);
+}
