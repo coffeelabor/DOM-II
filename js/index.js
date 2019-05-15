1bd0754305b194bf87fee5d0de5ccc951b7fd894
Reed James <ReedTJames1234@gmail.com>
2019-05-15 05:59:34 -0500

    WIP: another event added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Your code goes here
 
-const test = document.querySelector(".btn");
+const one = document.querySelector(".btn");
 
-test.addEventListener("mousemove", function(event) {
+one.addEventListener("mousemove", function(event) {
   alert("Sign Up!");
+});
+
+const two = document.querySelector("body, .nav-link, .main-navigation");
+
+two.addEventListener("dblclick", function(event) {
+  event.target.style.backgroundColor = "black";
+  event.target.style.color = "white";
 });
