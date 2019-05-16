8c34a45c2e46c3ab296568aabe651ec7fc3157ac
Reed James <ReedTJames1234@gmail.com>
2019-05-16 10:03:32 -0500

    WIP: ignore the randomChallenge.js file that got added

--- js/index.js
+++ js/index.js
@@ -12,10 +12,19 @@ two.addEventListener("dblclick", function(event) {
   event.target.style.backgroundColor = "black";
   event.target.style.color = "white";
 });
+//why doesnt this work
+// const three = document.getElementsByTagName("body");
+const three = document.querySelector("body");
+three.addEventListener("mousemove", function(event) {
+  let point = (event.offsetX, event.offsetY);
+  three.style.backgroundColor = point.toString(16);
+});
 
-const three = document.getElementsByTagName("body");
-three.addEventListener("mousemove", runEventThree);
+const fours = document.querySelectorAll(".btn");
 
-function runEventThree(event) {
-  console.log(event.offsetX, event.offsetY);
-}
+fours[1].addEventListener("mousedown", function(event) {
+  console.log("mousedown");
+  // if (fours.style.backgroundColor === "#17A2B8") {
+  //   fours.style.backgroundColor = "red";
+  // }
+});
---
+++ randomChallenge.js
@@ -0,0 +1,19 @@
+$_$wp(1);
+$_$w(1, 0), $_$tracer.log(capMe([
+    'mavis',
+    'senaida',
+    'letty'
+]), 'capMe([\n    \'mavis\',\n    \'senaida\',\n    ...', 1, 0);
+$_$w(1, 1), $_$tracer.log(capMe([
+    'samuel',
+    'MABELLE',
+    'letitia',
+    'meridith'
+]), 'capMe([\n    \'samuel\',\n    \'MABELLE\',\n   ...', 1, 1);
+$_$w(1, 2), $_$tracer.log(capMe([
+    'Slyvia',
+    'Kristal',
+    'Sharilyn',
+    'Calista'
+]), 'capMe([\n    \'Slyvia\',\n    \'Kristal\',\n   ...', 1, 2);
+$_$wpe(1);
